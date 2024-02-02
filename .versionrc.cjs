535005c16d1fcced23c96f4de167824dbf4a9d0c
Greg Fenton <greg.fenton@gmail.com>
2024-02-02 15:54:38 -0500

    update standard-version config

--- .versionrc.cjs
+++ .versionrc.cjs
@@ -1,7 +1,8 @@
 let bumps = [
   { filename: 'package.json' },
-  { filename: 'examples/simple-cli/package.json' },
-  { filename: 'examples/simple-react/package.json' },
+  { filename: 'examples/admin-cli/package.json' },
+  { filename: 'examples/client-cli/package.json' },
+  { filename: 'examples/client-react/package.json' },
 ]; // update the global project
 
 module.exports = {
